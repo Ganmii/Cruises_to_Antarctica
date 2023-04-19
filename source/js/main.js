85609ac0d1e2cb96642469ffd2b18dd4abd52646
Юрий <urijsaznev840@gmail.com>
2023-04-19 11:49:44 +0500

    Антарктика

--- source/js/main.js
+++ source/js/main.js
@@ -1,11 +1,14 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
-import {Form} from './modules/form-validate/form';
 
 // ---------------------------------
 
 window.addEventListener('DOMContentLoaded', () => {
 
+  if (map) {
+    mapImage.classList.add('contacts__image--hide');
+    createMap();
+  }
   // Utils
   // ---------------------------------
 
@@ -18,11 +21,72 @@ window.addEventListener('DOMContentLoaded', () => {
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
     initModals();
-    const form = new Form();
-    window.form = form;
-    form.init();
   });
 });
+
+const menuContainer = document.querySelector('.burger');
+const menu = menuContainer.querySelector('.burger__list');
+const links = document.querySelectorAll('.nav__link');
+const toggle = menuContainer.querySelector('.burger__toggle');
+const closingMenu = menuContainer.querySelector('.burger__close');
+const promo = document.querySelector('.promo');
+const map = document.querySelector('#map-canvas');
+const mapImage = map.querySelector('.contacts__image');
+const ymaps = window.ymaps;
+const MAP_CENTER = [59.938667, 30.323073];
+const ZOOM = 19;
+const PIN_SIZE = [18, 22];
+const PIN_OFSET = [-18, -22];
+
+// Бургер-меню
+
+menu.classList.remove('burger__list--nojs');
+toggle.classList.remove('burger__toggle--nojs');
+document.querySelector('.header__background').classList.remove('header__background--nojs');
+
+if (menuContainer) {
+  const toggleMenu = () => {
+    menuContainer.classList.toggle('burger--open');
+    document.body.classList.toggle('scroll-lock');
+    if (promo) {
+      promo.classList.toggle('promo--hidden');
+    }
+  };
+
+  const removeMenu = () => {
+    menuContainer.classList.remove('burger--open');
+    document.body.classList.remove('scroll-lock');
+    if (promo) {
+      promo.classList.remove('promo--hidden');
+    }
+  };
+
+  toggle.addEventListener('click', toggleMenu);
+  closingMenu.addEventListener('click', removeMenu);
+  links.forEach((el) => el.addEventListener('click', removeMenu));
+}
+
+// Яндекс.Карта
+
+const createMap = () => {
+  ymaps.ready(init);
+  function init() {
+    const newMap = new ymaps.Map(map, {
+      center: MAP_CENTER,
+      zoom: ZOOM,
+    });
+
+    const myPlacemark = new ymaps.Placemark(MAP_CENTER, {}, {
+      iconLayout: 'default#image',
+      iconImageHref: 'img/svg/pin.svg',
+      iconImageSize: PIN_SIZE,
+      iconImageOffset: PIN_OFSET,
+    });
+
+    newMap.geoObjects.add(myPlacemark);
+  }
+};
+
 
 // ---------------------------------
 
